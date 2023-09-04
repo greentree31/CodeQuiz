7b345f42da30390bc400c3b478efeeb2d3e40cc9
Melisha <melisha.evans06@gmail.com>
2023-09-04 00:07:06 -0700

    updated style

---
+++ assets/js/scripts.js
@@ -0,0 +1,31 @@
+//quiz questions
+var qcode = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: 'What does CSS stand for?',
+    choices: ['Cascading Style Sheets', 'Create Series Styles', 'Cascading Smart Styles', 'Calculating Sheet Styles'],
+    answer: 'Cascading Style Sheets',
+  },
+  {
+    title: 'What is HTML?',
+    choices: ['HTML stands for Higher Text Makeup Letters', 'HTML stands for Hyper Text Markup Language', 'HTML stands for Hydro Turbo Marker Lines', 'HTML stands for Hide The Media Lists'],
+    answer: 'HTML stands for Hyper Text Markup Language',
+  },
+  {
+    title:
+      "What does a paragraph tag look like.",
+    choices: ["<para>", "<paragraph>", "<p>", "<pgr>"],
+    answer: "<p>",
+  },
+  {
+    title:
+      "Can JavaScript change the style of an HTML element?",
+    choices: ["Yes", "No"],
+    answer: "Yes",
+  },
+];
+
